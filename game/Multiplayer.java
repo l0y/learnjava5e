bdbdc62da876d83808f73be91095c6decb25a89a
Marc Loy <ml004@loyinc.com>
2020-01-11 13:50:32 -0500

    Tweak volatiles per reader comment

--- game/Multiplayer.java
+++ game/Multiplayer.java
@@ -9,10 +9,10 @@ import java.net.SocketException;
 public class Multiplayer {
     private Field gameField;
     private int gamePort;
-    private boolean keepListening;
-    private boolean keepPlaying;
-    private boolean startNewGame;
-    private boolean disconnecting;
+    private volatile boolean keepListening;
+    private volatile boolean keepPlaying;
+    private volatile boolean startNewGame;
+    private volatile boolean disconnecting;
     private Server server;
     private Thread serverThread;
     private Thread clientThread;
